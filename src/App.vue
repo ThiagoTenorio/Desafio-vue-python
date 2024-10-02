e73ab46bfeef5e93d69c71b7cb91c23f485e886b
ThiagoTenorio <thiago_tenorio43@hotmail.com>
2024-10-02 13:39:26 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <BuscaMedicamentos></BuscaMedicamentos>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BuscaMedicamentos from "./components/BuscaMedicamentos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BuscaMedicamentos,
+  },
+};
 </script>
 
 <style>
